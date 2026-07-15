2110fcf599c668e34c4b639e327f28d7277bfc9d
Dewkith <dewkithrashmika@gmail.com>
2026-07-15 11:53:55 +0530

    feat: add user seeding script and improve password hashing logic

--- models/User.js
+++ models/User.js
@@ -49,11 +49,11 @@ const userSchema = new mongoose.Schema(
 
 
 // Pre-save hook to hash the password before saving the user
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+    
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 // Method to compare the provided password with the hashed password
---
+++ seed/user.seed.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const User = require("../models/User");
+
+const connectDB = require("../config/database");
+
+const seedUsers = async () => {
+  try {
+    await connectDB();
+
+    await User.deleteMany();
+
+    await User.create({
+      name: "System Admin",
+      email: "admin@cri.lk",
+      password: "Admin123",
+      role: "Admin",
+    });
+
+    await User.create({
+      name: "System Analyst",
+      email: "analyst@cri.lk",
+      password: "Analyst123",
+      role: "Analyst",
+    });
+
+    console.log("Users Seeded");
+
+    process.exit();
+  } catch (error) {
+    console.error(error);
+
+    process.exit(1);
+  }
+};
+
+seedUsers();
