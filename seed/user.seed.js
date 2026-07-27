8c8376db720ec1d8940ad812785806427658d5b3
Dewkith <dewkithrashmika@gmail.com>
2026-07-27 16:11:31 +0530

    chore: update user seed with real CRI estate manager emails

--- seed/user.seed.js
+++ seed/user.seed.js
@@ -12,32 +12,87 @@ const seedUsers = async () => {
     await User.deleteMany();
 
     await User.create([
-      {
-        name: "System Admin",
-        email: "admin@cri.lk",
-        password: "Admin123",
-        role: "Admin",
-      },
-      {
-        name: "System Analyst",
-        email: "analyst@cri.lk",
-        password: "Analyst123",
-        role: "Analyst",
-      },
-      {
-        name: "Ratmalagara Manager",
-        email: "ratmalagara.manager@cri.lk",
-        password: "Manager123",
-        role: "Estate Manager",
-      },
-      {
-        name: "Bandirippuwa Manager",
-        email: "bandirippuwa.manager@cri.lk",
-        password: "Manager123",
-        role: "Estate Manager",
-      },
-    ]);
+  {
+    name: "System Admin",
+    email: "admin@cri.lk",
+    password: "Admin123",
+    role: "Admin",
+  },
+  {
+    name: "System Analyst",
+    email: "analyst@cri.lk",
+    password: "Analyst123",
+    role: "Analyst",
+  },
 
+  // Estate Managers
+  {
+    name: "Bandirippuwa Manager",
+    email: "cri.bandirippuwaestate@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+  {
+    name: "Rathmalagara Manager",
+    email: "crirathmalagara@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+  {
+    name: "Ambakale Manager",
+    email: "agrc.abc.123@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+  {
+    name: "Poththukulama Manager",
+    email: "poththukulama.cri@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+  {
+    name: "Walpita Manager",
+    email: "criwalpita@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+  {
+    name: "Thabbowa Manager",
+    email: "thabbowaresearchcentercri@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+  {
+    name: "Middeniya Manager",
+    email: "middeniyacri@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+  {
+    name: "Makandura Manager",
+    email: "makanduragrc@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+  {
+    name: "Waligama Manager",
+    email: "sanjeewakumaraea@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+  {
+    name: "Maduruoya Manager",
+    email: "grcmaduruoya@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+  {
+    name: "Pallama Manager",
+    email: "cripallama123@gmail.com",
+    password: "Manager123",
+    role: "Estate Manager",
+  },
+]);
     console.log("Users seeded successfully.");
     process.exit(0);
   } catch (error) {
